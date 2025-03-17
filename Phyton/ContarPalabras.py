485a9931bbe7771c55da5f9465fba272ae4b8fab
Raúl <raulsarmientoperez@gmail.com>
2025-03-17 14:18:22 +0000

    Create ContarPalabras.py

---
+++ Phyton/ContarPalabras.py
@@ -0,0 +1,6 @@
+#contar palabras en Python
+oracion = input("Ingrese una oración: ")
+palabras = oracion.split()
+numero_de_palabras = len(palabras)
+
+print("Número de palabras en la oración:", numero_de_palabras)
