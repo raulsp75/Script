72033de3bc0868b42e7112ea197393c9d90e90ec
Raúl <raulsarmientoperez@gmail.com>
2025-03-18 11:26:46 +0000

    Create VerificaciónUsuario.py

---
+++ Phyton/VerificaciónUsuario.py
@@ -0,0 +1,9 @@
+#Verificación del usuario
+
+usu=str(input("Selecciona el nombre del usuario: "))
+con=str(input("Selecciona la contraseña: "))
+
+if usu == "pepe" and con == "asdasd":
+    print("Ha accedido correctamente")
+else:
+    print("El usuarioo la contraseña son incorrectos")
