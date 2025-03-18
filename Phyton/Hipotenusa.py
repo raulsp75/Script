024e16ec2a18a592ca63a8b47488a087ec4f405e
Raúl <raulsarmientoperez@gmail.com>
2025-03-18 11:14:59 +0000

    Create Hipotenusa.py

---
+++ Phyton/Hipotenusa.py
@@ -0,0 +1,8 @@
+import math
+a=float(input("Selecciona el valor de a= "))
+b=float(input("Selecciona el valor de b= "))
+a = a * a
+b = b * b
+c = math.sqrt(a+b)
+
+print("El valor de la hipotenusa es = ", c)
